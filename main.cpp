5c5fed2938c5c985e1c2afd1bdeeca3dc0f1a2a6
Twells2153 <43145319+Twells2153@users.noreply.github.com>
2019-11-02 16:54:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,39 +5,17 @@ using namespace std;
 
 // EDIT Made Connor
 
-int userArray();
-void populateArray(int);
-
 int main(){
 
     srand(time(0));
-    userArray();
-
-    return 0;
-}
-
-int userArray(){
-
     int SIZE;
-    int* myArray = 0;
-    do{
-        cout << "What array size? " << endl;
-        cin >> SIZE;
-        if (SIZE < 5)
-            cout << "Invalid arrays size. Size must >= 5." << endl;
-        else if (SIZE > 15)
-            cout << "Invalid arrays size. Size must <= 15" << endl;
-    } while ((SIZE < 5) || (SIZE > 15));
-
-    myArray = new int[SIZE];
-    for(int i = 0; i < SIZE; i++){
+    cout << "Enter SIZE -> ";
+    cin >> SIZE;
+    int myArray[SIZE];
+    for (int i = 0; i < SIZE; i++){
         cout << (rand() % 200)+10 << " ";
     }
-    return *myArray;
-}
 
-void displayReverse(int* myArray){
 
-    int SIZE;
-
+    return 0;
 }
